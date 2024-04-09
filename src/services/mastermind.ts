269f38e484e3e3682a6a23c5bcb9af09ee8c8d70
ajuggler <ajuggler@users.noreply.github.com>
2024-04-09 00:03:14 -0600

    Updated compression functions.  Added uncompression functions.

--- src/services/mastermind.ts
+++ src/services/mastermind.ts
@@ -459,6 +459,9 @@ function dec2bitArray(dec: any, length: number) {
   return Buffer.from(result);
 }
 
+
+//  COMPRESSION FUNCTIONS
+
 async function compressedG1(point: Array<string | bigint>) {
   const curve = await ff.getCurveFromName("bls12381");
 
@@ -477,15 +480,18 @@ async function compressedG1(point: Array<string | bigint>) {
     const x = F.fromObject(BigInt(point[0]));
 
     const x3b = F.add(F.mul(F.square(x), x), curve.G1.b);
-    const y1 = F.toObject(F.sqrt(x3b));
-    const y2 = F.toObject(F.neg(F.sqrt(x3b)));
 
-    const y = BigInt(point[1]);
+    const y = F.fromObject(BigInt(point[1]));
+    const y2 = F.square(y);
 
-    if (y1 > y2 && y > y2) {
-      result[0] = result[0] | YBIT;
-    } else if (y1 < y2 && y > y1) {
-      result[0] = result[0] | YBIT;
+    if (F.eq(y2, x3b)) {
+      const ya = F.toObject(y);
+      const yb = F.toObject(F.neg(y));
+      if (ya > yb) {
+	result[0] = result[0] | YBIT;
+      }
+    } else {
+      throw new Error("point not on G1");
     }
   }
 
@@ -495,9 +501,13 @@ async function compressedG1(point: Array<string | bigint>) {
 async function compressedG2(point: Array<Array<string | bigint>>) {
   const curve = await ff.getCurveFromName("bls12381");
 
+  // const result = Buffer.concat([                  * NOTE:  reverted the order *
+  // 	bb.toBufferBE(BigInt(point[0][1]), 48),
+  // 	bb.toBufferBE(BigInt(point[0][0]), 48),
+  // ]);
   const result = Buffer.concat([
-    bb.toBufferBE(BigInt(point[0][1]), 48),
     bb.toBufferBE(BigInt(point[0][0]), 48),
+    bb.toBufferBE(BigInt(point[0][1]), 48)
   ]);
   const COMPRESSED = 0b10000000;
   const INFINITY = 0b01000000;
@@ -512,21 +522,19 @@ async function compressedG2(point: Array<Array<string | bigint>>) {
 
     const x = F.fromObject(point[0].map((item) => BigInt(item)));
 
-    // console.log("x", x);
-
     const x3b = F.add(F.mul(F.square(x), x), curve.G2.b);
-    const y1 = F.toObject(F.sqrt(x3b));
-    const y2 = F.toObject(F.neg(F.sqrt(x3b)));
-    // console.log("y1", y1);
-    // console.log("y2", y2);
-    // console.log("point", point[1]);
 
-    const y = point[1].map((item) => BigInt(item));
+    const y = F.fromObject(point[1].map((item) => BigInt(item)));
+    const y2 = F.square(y);
 
-    if (greaterThan(y1, y2) && greaterThan(y, y2)) {
-      result[0] = result[0] | YBIT;
-    } else if (greaterThan(y2, y1) && greaterThan(y, y1)) {
-      result[0] = result[0] | YBIT;
+    if (F.eq(y2, x3b)) {
+      const ya = F.toObject(y);
+      const yb = F.toObject(F.neg(y));
+      if (greaterThan(ya, yb)) {
+	result[0] = result[0] | YBIT;
+      }
+    } else {
+      throw new Error("point not on G2");
     }
   }
   return result.toString("hex");
@@ -541,14 +549,46 @@ function greaterThan(a: Array<BigInt>, b: Array<BigInt>) {
   return false;
 }
 
-function uncompressedG1(point: string) {
+function bytesToBigInt(bytes: number[]): bigint {
+  const l = bytes.length;
+
+  let result = BigInt(0);
+  for (let i = 0; i < bytes.length; i++) {
+    // Shift each byte and add it to the result
+    result += BigInt(bytes[i]) << BigInt((l - i - 1) * 8);
+  }
+
+  return result;
+}
+
+function bytesUntaggedToBigInt(bytes: number[]): bigint {
+  const l     = bytes.length;
+  const UNTAG = 0b00011111;
+
+  // Shift untagged first byte
+  let result = BigInt(bytes[0] & UNTAG) << BigInt((l - 1) * 8);
+  for (let i = 1; i < bytes.length; i++) {
+    // Shift each byte and add it to the result
+    result += BigInt(bytes[i]) << BigInt((l - i - 1) * 8);
+  }
+
+  return result;
+}
+
+
+// UNCOMPRESSION FUNCTIONS
+
+async function uncompressedG1(point: string) {
+  const curve = await ff.getCurveFromName("bls12381");
+  const F = curve.G1.F;
+
   const COMPRESSED = 0b10000000;
   const INFINITY = 0b01000000;
   const YBIT = 0b00100000;
 
   const buffer = Buffer.from(point, "hex");
 
-  if ((buffer[0] & COMPRESSED) === 0) {
+  if (buffer.length != 48 || (buffer[0] & COMPRESSED) === 0) {
     throw new Error("Invalid format");
   }
 
@@ -556,23 +596,62 @@ function uncompressedG1(point: string) {
     return [BigInt(1), BigInt(1), BigInt(0)];
   }
 
-  return [BigInt(1), BigInt(1), BigInt(0)]; // TODO
+  const xn  = bytesUntaggedToBigInt(buffer);
+  const x   = F.fromObject(xn);
+  const x3b = F.add(F.mul(F.square(x), x), curve.G1.b);
+
+  const y  = F.sqrt(x3b);
+  const y1 = F.toObject(y);
+  const y2 = F.toObject(F.neg(y));
+
+  let yn;
+  if (buffer[0] & YBIT) {
+    yn = y1 > y2 ? y1 : y2;
+  } else {
+    yn = y1 > y2 ? y2 : y1;
+  }
+
+  return [xn, yn, BigInt(1)];
 }
 
-function uncompressedG2(point: string) {
+async function uncompressedG2(point: string) {
+  const curve = await ff.getCurveFromName("bls12381");
+  const F = curve.G2.F;
+
   const COMPRESSED = 0b10000000;
   const INFINITY = 0b01000000;
   const YBIT = 0b00100000;
 
   const buffer = Buffer.from(point, "hex");
 
-  if ((buffer[0] & COMPRESSED) === 0) {
+  if (buffer.length != 96 || (buffer[0] & COMPRESSED) === 0) {
     throw new Error("Invalid format");
   }
 
-  return [
-    [BigInt(1), BigInt(1)],
-    [BigInt(1), BigInt(1)],
-    [BigInt(0), BigInt(0)],
-  ]; // TODO
+  if ((buffer[0] & INFINITY) !== 0) {
+    return [
+      [BigInt(1), BigInt(0)], [BigInt(1), BigInt(0)], [BigInt(0), BigInt(0)]
+    ];
+  }
+
+  const xn  = [
+    bytesUntaggedToBigInt(buffer.slice(0, 48)),
+    bytesToBigInt(buffer.slice(48))
+  ];
+
+  const x   = F.fromObject(xn);
+  const x3b = F.add(F.mul(F.square(x), x), curve.G2.b);
+
+  const y  = F.sqrt(x3b);
+  const y1 = F.toObject(y);
+  const y2 = F.toObject(F.neg(y));
+
+  let yn;
+  if (buffer[0] & YBIT) {
+    yn = greaterThan(y1, y2) ? y1 : y2;
+  } else {
+    yn = greaterThan(y1, y2) ? y2 : y1;
+  }
+
+  return [xn, yn, [BigInt(1), BigInt(0)]]
 }
