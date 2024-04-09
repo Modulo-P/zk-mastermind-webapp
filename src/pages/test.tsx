269047581ace6add49be901f198931f954b432db
ajuggler <ajuggler@users.noreply.github.com>
2024-04-09 00:07:22 -0600

    cleanup

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -6,7 +6,7 @@ export default function Test() {
     const ff = require("ffjavascript");
     const bb = require("bigint-buffer");
 
-    //  TEST EXAMPLE:  G1 generator
+    //  TEST EXAMPLE:  G1 & G2 generators
     const g1_gen = '97f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb';
     const g2_gen = '824aa2b2f08f0a91260805272dc51051c6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb813e02b6052719f607dacd3a088274f65596bd0d09920b61ab5da61bbdc7f5049334cf11213945d57e5ac7d055d042b7e';
 
